c821f9b1bc95b75024c0dd621b6d060a768fe6c6
i1620 <68018020+i1620@users.noreply.github.com>
2020-09-28 12:50:38 -0500

    made some changes

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Dependencies
 import datetime as dt
 from dateutil.parser import parse
 import numpy as np
@@ -31,11 +32,12 @@ app = Flask(__name__)
 measurement_df = pd.read_sql("select date, prcp from Measurement \
     where date between '2016-08-23' and '2017-08-23'",conn)
 measurement_df = measurement_df.dropna()
+prcp_df = (measurement_df.to_dict(orient='records'))
 station_df = pd.read_sql("select * from Station",conn)
 station_list = station_df['station'].to_json(orient='records')
 high_station = pd.read_sql("select date,tobs from Measurement \
-where station = 'USC00519281' and date between '2016-08-23' \
-    and '2017-08-23'", conn)
+    where station = 'USC00519281' and date between '2016-08-23' \
+        and '2017-08-23'", conn)
 station_9281 = high_station.to_json(orient='records')
 measure_df = pd.read_sql("select * from Measurement",conn)
 last_year = measure_df.iloc[-1]['date']
@@ -50,32 +52,46 @@ def hello():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+        f"Thank you for visiting!<br/>"
     )
 
-@app.route('/api/v1.0/precipitation')
+# Precipitation API
+@app.route("/api/v1.0/precipitation")
 def precipitation():
-    """Return a list of stations."""
-    return jsonify(measurement_df.to_dict(orient='records'))
 
-@app.route('/api/v1.0/stations')
-def stations():
     """Return a list of stations."""
+
+    return jsonify(prcp_df)
+
+# Stations API
+@app.route("/api/v1.0/stations")
+def stations():
+
+    """Return a list of stations."""
+
     return station_list
 
-@app.route('/api/v1.0/tobs')
-def temp_monthly():
+# Temperature API
+@app.route("/api/v1.0/tobs")
+def temperatures():
+
     """Return the temperature observations (tobs) for previous year."""
+
     return station_9281
 
-@app.route('/api/v1.0/<start>')
-@app.route('/api/v1.0/<start>/<end>')
-def stats(start=None, end=None):
+# Dates Data API
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+def dates(start=None, end=None):
+
     """Return TMIN, TAVG, TMAX."""
+
     if not end:
         end = last_year
-    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), 
-    func.max(Measurement.tobs)).filter(Measurement.date >= start).filter\
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), \
+        func.max(Measurement.tobs)).filter(Measurement.date >= start).filter\
             (Measurement.date <= end).all()
     return jsonify(results)
 
